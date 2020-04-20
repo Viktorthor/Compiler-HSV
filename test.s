09ecfec5b5e2c72d18920b312cc210cb9e9bdf2c
SonVan <32542221+SonVan@users.noreply.github.com>
2020-04-20 22:23:28 +0000

    test eins og í handbók

--- test.s
+++ test.s
@@ -1,16 +1,13 @@
-// test comment
-
 test()
 {
 	var a,b;
 	a = 2+3;
 	b = "HelloWorld";
-	
 	if(a == 5) {
-		writeln(a);
+		writeln(a); 
 		writeln(b);
 	};
-	
+
 	if(a != 5) {
 		writeln("if a is != 5");
 	}
@@ -20,18 +17,29 @@ test()
 	else {
 		writeln("else a is >= 5");
 	};
-	
+
 	return "Return value is " ++ 1;
 }
+test2(x, y)
+{
+	var i;
+	i = 0;
+	while(i != 5){
+		i = i + 1;
+	};
 
+	if(i == 5) {
+		writeln("x equals " ++ x ++ " and y equals " ++ y);
+	};
+}
 main()
 {
-	var x,i;
+	var i;
 	writeln(test());
-	
+	test2(2, 5);
 	i = 0;
 	while(i <= 3) {
 		writeln("while loop " ++ i);
 		i = i+1;
-	};	
+	};
 }
